8bf7db6852b32270355e10a3328d49a29f7ba747
Roberto Carrión <robertocg051096@gmail.com>
2025-02-18 17:25:06 +0100

    Update TarjetaCredito.java

--- TarjetaCredito.java
+++ TarjetaCredito.java
@@ -1,23 +1,27 @@
 package org.example.practica1;
 
+import java.util.ArrayList;
+
 public class TarjetaCredito extends MetodoPago{
 
     private String nro_tarjeta;
-    private String tipo[] = {"VISA", "MASTERCARD", "MAESTRO"};
+    private ArrayList<TarjetaCredito> tipo;
 
-    public TarjetaCredito(double importe) {
+    public TarjetaCredito(double importe, String nro_tarjeta) {
         super(importe);
-    }
-
-    public void procesarPago() {
-
+        this.nro_tarjeta = nro_tarjeta;
+        tipo = new ArrayList<>();
     }
 
     public void procesarPago(double importe) {
         System.out.println("Procesando pago de " + importe + " € con tarjeta de crédito VISA");
     }
 
-    public void validarTarjeta(){
-        
+    public boolean validarTarjeta(String nro_tarjeta){
+        if (nro_tarjeta.length() != 16) {
+            System.out.println("ERROR. Tiene que ser de 16 caracteres");
+            return false;
+        }
+        return true;
     }
 }
