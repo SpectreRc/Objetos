67fdcbb8d56fa13cc0dff59453a36be6fbfe270f
Roberto Carrión <robertocg051096@gmail.com>
2025-02-19 17:05:44 +0100

    Update AppEcommerce.java

--- AppEcommerce.java
+++ AppEcommerce.java
@@ -2,13 +2,6 @@ package org.example.practica1;
 
 public class AppEcommerce {
     public static void main(String[] args) {
-        MetodoPago tarjeta = new TarjetaCredito("1234567890123456", "VISA");
-        MetodoPago paypal = new PayPal("roberto@gmail.com");
-        MetodoPago bizum = new Bizum("666777888");
-
-        tarjeta.procesarPago(50.0);
-        paypal.procesarPago(30.0);
-        bizum.procesarPago(20.0);
 
         Tienda.iniciarPago();
     }
