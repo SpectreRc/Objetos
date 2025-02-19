2bc74ddb9106c990eefaadb7b7b7b0271d5b4d05
Roberto Carrión <robertocg051096@gmail.com>
2025-02-19 12:29:02 +0100

    Update Bizum.java

--- Bizum.java
+++ Bizum.java
@@ -1,12 +1,31 @@
 package org.example.practica1;
 
-public class Bizum extends MetodoPago{
+import java.util.Random;
 
-    public Bizum(double importe) {
-        super(importe);
+public class Bizum extends MetodoPago {
+
+    Random random = new Random();
+    private String telefono;
+    private int pin;
+
+    public Bizum(String telefono) {
+        this.telefono = telefono;
+        this.pin = generarPin();
     }
 
-    public void procesarPago() {
+    public void procesarPago(double importe) {
+        System.out.println("Procesando pago de " + importe + " € con Bizum");
+    }
 
+    public boolean validarBizum(int pinIngresado) {
+        return telefono.matches("\\d{9}") && pinIngresado == pin;
+    }
+
+    public int generarPin() {
+        int nuevoPin = random.nextInt(1000000);
+        if (nuevoPin < 100000 || nuevoPin > 999999) {
+            System.out.println("Error. El pin no es válido");
+        }
+        return nuevoPin;
     }
 }
